0b5bbf6a7e2f228ec6f71246ff2f1e87c8ed135c
Ando Roots <david@sqroot.eu>
2013-12-21 22:44:13 +0200

    Pretify the home page

--- lib/pages/public_profile.rb
+++ lib/pages/public_profile.rb
@@ -16,6 +16,7 @@ class PublicProfile
 
   end
 
+  # Extract Xpath val from the page and convert to type
   def get_val(xpath, type)
     case type
       when 'int'
@@ -25,31 +26,38 @@ class PublicProfile
       else
         @page.xpath(xpath).text.strip
     end
-
   end
 
+  # Recursively extract data data from the page as defined by the config item
   def parse_item(item)
 
+    # Extract from page
     if item.has_key?('path')
       return get_val(item['path'], item['type'])
     end
 
+    # Dealing with a nested item here
     g1 = {}
     item.each do |v1, item|
       g1[v1] = parse_item(item)
     end
     g1
+
   end
 
+  # Extract values from the page
   def parse
 
     values = {}
+
+    # Items are defined in the config file
     paths = YAML.load_file('config/paths.yml')
 
     paths.each do |key, item|
       values[key] = parse_item(item)
     end
 
+    # Additional values
     values['account'].merge!({
                                 world: @world,
                                 username: @username,
@@ -59,6 +67,7 @@ class PublicProfile
     values
   end
 
+  # Full profile URL for crime.ee
   def self.profile_url(world, username)
     BASE_URL.gsub('{world}', world).gsub('{user}', Rack::Utils.escape(username))
   end
