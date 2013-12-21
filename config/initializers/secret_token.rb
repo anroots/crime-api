7be926a6251ee4703ae7df944fb340addfc3ec9a
Ando Roots <david@sqroot.eu>
2013-12-21 13:31:04 +0200

    Fix app constant name in secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,7 +9,7 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-MyApp::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+CrimeApi::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
                                            'aa79975ceac09291266d0949ba4e9301c73adbd3435160d5ac66b576d348eadc275468dd5b4408227e37008f89dd9c36e901514393ee0f30a134367f27f0ddda'
                                          else
                                            ENV['SECRET_TOKEN']
