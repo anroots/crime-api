4c1515c3b4b0c3bb4904158b2e06f55b4ca2f5c7
Ando Roots <david@sqroot.eu>
2013-12-21 22:05:03 +0200

    Put data xpath info into a config file

--- lib/pages/public_profile.rb
+++ lib/pages/public_profile.rb
@@ -16,52 +16,51 @@ class PublicProfile
 
   end
 
+  def get_val(xpath, type)
+    case type
+      when 'int'
+        @page.xpath(xpath).text.to_s.gsub(/[[:space:]]/, '').to_i
+      when 'bool'
+        @page.xpath(xpath).text.strip.downcase == YES
+      else
+        @page.xpath(xpath).text.strip
+    end
+
+  end
+
+  def parse_item(item)
+
+    if item.has_key?('path')
+      return get_val(item['path'], item['type'])
+    end
+
+    g1 = {}
+    item.each do |v1, item|
+      g1[v1] = parse_item(item)
+    end
+    g1
+  end
 
   def parse
-    values = {
-          world: @world,
-          username: @username,
-          profile_url: PublicProfile.profile_url(@world,@username)
-    }
 
-    @numeric_values = {
-        mining: '//*[@id="centerbar"]/div/table/tr[18]/td[2]/strong',
-        fame:'//*[@id="centerbar"]/div/table/tr[10]/td[1]/strong'
-    }
+    values = {}
+    paths = YAML.load_file('config/paths.yml')
 
-    @string_values = {
-        world: '//*[@id="centerbar"]/div/table/tr[3]/td/strong',
-        signup_date: '//*[@id="centerbar"]/div/table/tr[4]/td/strong',
-        online_date: '//*[@id="centerbar"]/div/table/tr[5]/td/strong'
-    }
-
-    @boolean_values = {
-        banned: '//*[@id="centerbar"]/div/table/tr[8]/td/strong',
-        vip: '//*[@id="centerbar"]/div/table/tr[6]/td/strong',
-        has_clan: '//*[@id="centerbar"]/div/table/tr[7]/td/strong'
-    }
-
-
-    # Numeric values
-    @numeric_values.each do |key, xpath|
-      values[key] = @page.xpath(xpath).text.to_s.gsub(/[[:space:]]/, '').to_i
+    paths.each do |key, item|
+      values[key] = parse_item(item)
     end
 
-    # Strings
-    @string_values.each do |key, xpath|
-      values[key] = @page.xpath(xpath).text.strip
-    end
-
-    # Booleans
-    @boolean_values.each do |key, xpath|
-      values[key] = @page.xpath(xpath).text.strip.downcase == YES
-    end
+    values['account'].merge({
+                                world: @world,
+                                username: @username,
+                                profile_url: PublicProfile.profile_url(@world, @username)
+                            })
 
     values
   end
 
   def self.profile_url(world, username)
-    BASE_URL.gsub('{world}', world).gsub('{user}',Rack::Utils.escape(username))
+    BASE_URL.gsub('{world}', world).gsub('{user}', Rack::Utils.escape(username))
   end
 
 
