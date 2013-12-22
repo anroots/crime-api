e366a2352539f61c61088d23d0ac8121322e96df
Ando Roots <david@sqroot.eu>
2013-12-22 13:02:51 +0200

    Add controller action to output all players in the DB

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,13 +1,13 @@
 class PlayersController < ApplicationController
 
-  def index
-
+  before_action do
     # Require API key
     unless params.has_key?('api_key') and params[:api_key] == 'demo'
       render json: {response: 'API key invalid'}, :status => :unauthorized
-      return
     end
+  end
 
+  def index
     require 'pages/public_profile'
 
     begin
@@ -17,6 +17,24 @@ class PlayersController < ApplicationController
       render json: {response: 'User Not Found!'}, :status => :not_found
     end
 
+  end
+
+  # List all players by world
+  def all
+
+    players = OnlinePlayer.uniq.find_all
+
+    usernames = {}
+    players.each do |player|
+
+      unless usernames.has_key?(player.world)
+        usernames[player.world] = []
+      end
+
+      usernames[player.world].push(player.username)
+    end
+
+    render json: usernames
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,11 @@ CrimeApi::Application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
+  get 'players/all', to: 'players#all'
+
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :players, defaults: {format: :json}
+
 
   # Example resource route with options:
   #   resources :products do
